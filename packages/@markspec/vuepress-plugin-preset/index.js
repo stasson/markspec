98f04fa649f18889588ff7d690786009993a84aa
stasson <sebastien.tasson@gmail.com>
2019-04-08 23:37:58 +0200

    feat(vuepress-plugin-preset): add vuepress image plugin

--- packages/@markspec/vuepress-plugin-preset/index.js
+++ packages/@markspec/vuepress-plugin-preset/index.js
@@ -3,6 +3,7 @@ module.exports = {
   plugins: [
     require("@markspec/vuepress-plugin-plantuml"),
     require("@markspec/vuepress-plugin-katex"),
-    require("@markspec/vuepress-plugin-footnote")
+    require("@markspec/vuepress-plugin-footnote"),
+    require("@markspec/vuepress-plugin-image")
   ]
 };
