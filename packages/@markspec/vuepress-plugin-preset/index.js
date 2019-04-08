4aa2bdce4b7334941240acae2230813796d80286
stasson <sebastien.tasson@gmail.com>
2019-04-08 18:54:21 +0200

    feat(vuepress-plugin-preset): add support for footnotes

--- packages/@markspec/vuepress-plugin-preset/index.js
+++ packages/@markspec/vuepress-plugin-preset/index.js
@@ -2,6 +2,7 @@ module.exports = {
   name: "@markspec/vuepress-plugin-preset",
   plugins: [
     require("@markspec/vuepress-plugin-plantuml"),
-    require("@markspec/vuepress-plugin-katex")
+    require("@markspec/vuepress-plugin-katex"),
+    require("@markspec/vuepress-plugin-footnote")
   ]
 };
