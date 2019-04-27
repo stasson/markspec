74d30b107ab7fb103c4bf6f490298e567253c87e
stasson <sebastien.tasson@gmail.com>
2019-04-27 07:27:43 +0200

    refactor(vuepress-theme-default): extend default vuepress theme

--- packages/@markspec/vuepress-theme-default/index.js
+++ packages/@markspec/vuepress-theme-default/index.js
@@ -3,43 +3,6 @@ const pluginPreset = require('@markspec/vuepress-plugin-preset')
 
 // Theme API.
 module.exports = (options, ctx) => ({
-  alias () {
-    const { themeConfig, siteConfig } = ctx
-    // resolve algolia
-    const isAlgoliaSearch = (
-      themeConfig.algolia
-      || Object.keys(siteConfig.locales && themeConfig.locales || {})
-        .some(base => themeConfig.locales[base].algolia)
-    )
-    return {
-      '@AlgoliaSearchBox': isAlgoliaSearch
-        ? path.resolve(__dirname, 'components/AlgoliaSearchBox.vue')
-        : path.resolve(__dirname, 'noopModule.js')
-    }
-  },
-
-  plugins: [
-    pluginPreset,
-    ['@vuepress/active-header-links', options.activeHeaderLinks],
-    '@vuepress/search',
-    '@vuepress/plugin-nprogress',
-    ['container', {
-      type: 'tip',
-      defaultTitle: {
-        '/zh/': '提示'
-      }
-    }],
-    ['container', {
-      type: 'warning',
-      defaultTitle: {
-        '/zh/': '注意'
-      }
-    }],
-    ['container', {
-      type: 'danger',
-      defaultTitle: {
-        '/zh/': '警告'
-      }
-    }],
-  ]
+  extend: '@vuepress/theme-default',
+  plugins: [pluginPreset]
 })
