293b168ca87bc5278ee9b9fca958ec40d88ef80c
stasson <sebastien.tasson@gmail.com>
2019-09-22 11:32:53 +0200

    feat(vuepress-theme-default): page-break component

---
+++ packages/@markspec/vuepress-theme-default/components/page-break.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="pagebreak" />
+</template>
+
+<script>
+export default {
+  name: 'page-break'
+}
+</script>
+
+<style>
+.pagebreak {
+  page-break-after: always;
+}
+</style>
