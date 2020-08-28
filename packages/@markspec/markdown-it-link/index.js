d496f6cffcd601db7d9c4670f517796999e64be1
stasson <sebastien.tasson@gmail.com>
2020-08-28 06:55:01 +0200

    refactor(markdown-it-link): remove extra comments

--- packages/@markspec/markdown-it-link/index.js
+++ packages/@markspec/markdown-it-link/index.js
@@ -3,20 +3,6 @@ const externalRE = /^https?:/;
 const sourceRE = /(\/|\.md|\.html)(#.*)?$/;
 
 module.exports = function (md) {
-  // md.core.ruler.after("inline", "replace-link", function (state) {
-  //   state.tokens.forEach(function (blockToken) {
-  //     if (blockToken.type === "inline" && blockToken.children) {
-  //       blockToken.children.forEach(function (token) {
-  //         const type = token.type;
-  //         if (type === "link_open") {
-  //           replaceAttr(token, "href", replaceLink, state.env);
-  //         }
-  //       });
-  //     }
-  //   });
-  //   return false;
-  // });
-
   md.renderer.rules.link_open = (tokens, idx, options, env, self) => {
     const token = tokens[idx];
     const hrefIndex = token.attrIndex("href");
@@ -24,24 +10,26 @@ module.exports = function (md) {
       const link = token.attrs[hrefIndex];
       const href = link[1];
       if (externalRE.test(href)) {
-        // blank target here
-        var aIndex = tokens[idx].attrIndex('target')
+        // IF external url: set blank target
+        var aIndex = tokens[idx].attrIndex("target");
         if (aIndex < 0) {
-          tokens[idx].attrPush(['target', '_blank']) // add new attribute
+          tokens[idx].attrPush(["target", "_blank"]);
         }
       } else if (sourceRE.test(href)) {
-        // remplace md with html
+        // IF NOT external and source url
         if (indexRE.test(href)) {
+          // AND readme url: set link to `index.html`
           link[1] = href
             .replace(/readme\.md$/i, "index.html")
             .replace(/readme\.md(#.*)$/i, "index.html$1");
         } else {
+          // ELSE: set html link
           link[1] = href
             .replace(/\.md$/, ".html")
             .replace(/\.md(#.*)$/, ".html$1");
         }
       }
     }
-    return self.renderToken(tokens, idx, options)
+    return self.renderToken(tokens, idx, options);
   };
 };
